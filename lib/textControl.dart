ccbd4b0756babbd7e6dbc5d5dae9b0cb02564dfa
Gaurav <shahgauravraj@gmail.com>
2020-09-07 22:16:57 +0545

    added another custom widget

---
+++ lib/textControl.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextControl extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
