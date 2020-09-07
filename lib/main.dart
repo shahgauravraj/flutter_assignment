1475c23d7f4802cad5ae0f00b5d6b4647af6f188
Gaurav <shahgauravraj@gmail.com>
2020-09-07 22:17:44 +0545

    modified main file for two external custom widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,36 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
 import 'package:flutter/material.dart';
+import './texts.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _textIndex = 0;
+  void _changeText() {
+    setState(() {
+      _textIndex += 1;
+    });
+    print(_textIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var texts = [
+      'This is first text',
+      'This is second text',
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -14,14 +40,12 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text(
-              'This is the statement to be displayed at the top.',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 26),
+            Texts(
+              texts[_textIndex],
             ),
             RaisedButton(
-              onPressed: () => print('Button pressed once.'),
               child: Text('Press Here'),
+              onPressed: _changeText,
             )
           ],
         ),
