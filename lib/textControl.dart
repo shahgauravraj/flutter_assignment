1e3cd5970e7320a4b7ac4508e4d0e0357d34ba8d
Gaurav <shahgauravraj@gmail.com>
2020-09-08 14:49:35 +0545

    modified this widget to change text displayed

--- lib/textControl.dart
+++ lib/textControl.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TextControl extends StatelessWidget {
+  final Function selectHandler;
+
+  TextControl(this.selectHandler);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: RaisedButton(
+        color: Colors.blueAccent,
+        textColor: Colors.white,
+        child: Text('Press Here'),
+        onPressed: selectHandler,
+      ),
     );
   }
 }
