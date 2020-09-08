d3f8ce4eb48d014a1c710004c701c9b15a5c1bba
Gaurav <shahgauravraj@gmail.com>
2020-09-08 14:50:52 +0545

    built a widget for displaying different texts

--- lib/texts.dart
+++ lib/texts.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-
-class  Texts extends StatelessWidget {
+class Texts extends StatelessWidget {
   final String variableTexts;
 
   Texts(this.variableTexts);
 
   @override
   Widget build(BuildContext context) {
-    return Text(variableTexts);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        variableTexts,
+        style: TextStyle(fontSize: 26),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
